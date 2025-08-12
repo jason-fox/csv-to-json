2e3769d89a85480a919660703a6555b5bf1f5292
Bernard Okpattah <bernardokpattah@BKO-2021.local>
2025-08-12 14:49:44 +0000

    updated upload api endpoint

--- controllers/api.js
+++ controllers/api.js
@@ -18,21 +18,35 @@ function formatResponseData(contributor, model, externalUrl, data) {
         switch (contributor) {
             case 'ayogreen':
                 if (model === 'parcel') {
-                  return data.map((d) => {
-                        const dd = d;
-                        for (const [key, value] of Object.entries(dd)) {
-                            if (value === null || value === undefined || value === '') {
-                                delete dd[key]
+                    return data.map((dd) => {
+                        const entity = {
+                            type: 'AgriParcel'
+                        };
+                        for (const [k, v] of Object.entries(dd)) {
+                            if (v === null || v === undefined || v === '') {
                                 continue;
                             }
+                            if (k === 'id' || k === 'updated_at' || k === 'created_at') {
+                                switch (k) {
+                                    case 'id':
+                                        entity[k] = `urn:ngsi-ld:AgriParcel:${v}`;
+                                        break;
+                                    default:
+                                        entity[k] = v;
+                                }
+                                continue;
+                            }
+                            entity[k] = {
+                                type: 'Property',
+                                value: v
+                            };
                         }
                         return {
-                            ...dd,
-                            id: `urn:ngsi-ld:AgriParcel:${dd.id}`,
-                            dateCreated: dd.created_at,
-                            dateModified: dd.updated_at,
-                            type: 'AgriParcel',
-                            seeAlso: [externalUrl]
+                            ...entity,
+                            seeAlso: {
+                                type: 'Property',
+                                value: [externalUrl]
+                            }
                         };
                     });
                 }
@@ -45,11 +59,13 @@ function formatResponseData(contributor, model, externalUrl, data) {
         return null;
     }
 }
+
+
 /**
  * Actions when uploading a API. The API holds an array of
  * measurements each at a given timestamp.
  */
-const upload = async (req, res) => {
+async function upload(req, res) {
     // get token from authorization header
     const requestToken = req.headers.authorization;
 
@@ -64,7 +80,7 @@ const upload = async (req, res) => {
     if (!formattedData) {
         return res.status(400).send({ message: 'response could not be formatted', data });
     }
-   // return res.status(200).send({ message: 'data fetched from api', formattedData });
+    // return res.status(200).send({ message: 'data fetched from api', formattedData });
     //Send data to context broker
     const cbResponse = await Measure.sendAsHTTP(formattedData);
     if (cbResponse && (cbResponse.statusCode === 200 || cbResponse.statusCode === 201)) {
@@ -73,7 +89,7 @@ const upload = async (req, res) => {
     return res
         .status(cbResponse ? cbResponse.statusCode : 204)
         .send({ message: 'error sending data to context broker', formattedData });
-};
+}
 
 module.exports = {
     upload
